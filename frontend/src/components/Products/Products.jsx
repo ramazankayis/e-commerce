55f1f8d508d6becd20b630743f9d7be3bf1c34f6
Ramazan KAYIŞ <kayis.ramazan@gmail.com>
2023-12-26 22:34:21 +0300

    to the project added the React Slick component

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -1,6 +1,52 @@
+import { useState } from "react";
 import ProductItem from "./ProductItem";
+import ProductsData from "../../data.json";
+import Slider from "react-slick";
 import "./Products.css";
+
+const NextBtn = ({ onClick }) => {
+  return (
+    <button className="glide__arrow glide__arrow--right" onClick={onClick}>
+      <i className="bi bi-chevron-right"></i>
+    </button>
+  );
+};
+
+const PrevBtn = ({ onClick }) => {
+  return (
+    <button className="glide__arrow glide__arrow--left" onClick={onClick}>
+      <i className="bi bi-chevron-left"></i>
+    </button>
+  );
+};
 const Products = () => {
+  const [products, setProducts] = useState(ProductsData);
+
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <NextBtn />,
+    prevArrow: <PrevBtn />,
+    autoplaySpeed: 1500,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <section className="products">
       <div className="container">
@@ -9,22 +55,13 @@ const Products = () => {
           <p>Summer Collection New Morden Design</p>
         </div>
         <div className="product-wrapper product-carousel">
-          <div className="glide__track">
-            <ul className="product-list glide__slides" id="product-list">
-             <ProductItem/>
-             <ProductItem/>
-             <ProductItem/>
-             <ProductItem/>
-            </ul>
-          </div>
-          <div className="glide__arrows">
-            <button className="glide__arrow glide__arrow--left">
-              <i className="bi bi-chevron-left"></i>
-            </button>
-            <button className="glide__arrow glide__arrow--right">
-              <i className="bi bi-chevron-right"></i>
-            </button>
-          </div>
+          <Slider {...sliderSettings}>
+            {products.map((product) => (
+              <ProductItem product={product} key={product.id} />
+            ))}
+          </Slider>
+
+          <div className="glide__arrows"></div>
         </div>
       </div>
     </section>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import MainLayout from "./Layouts/MainLayout.jsx";
 import "./index.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <MainLayout>
