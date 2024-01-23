02cf610e7d5f3cd1893efb62a674d781cc711f08
Ramazan KAYIŞ <kayis.ramazan@gmail.com>
2024-01-23 18:02:01 +0300

    the  cart component updated

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -2,24 +2,31 @@ import CartProgress from "./CartProgress";
 import CartTable from "./CartTable";
 import CartCoupon from "./CartCoupon";
 import "./Cart.css";
- import CartTotals from "./CartTotals";
- 
+import CartTotals from "./CartTotals";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartProvider";
+
 const Cart = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <section className="cart-page">
       <div className="container">
-        <div className="cart-page-wrapper">
-          <form className="cart-form">
-            <CartProgress />
-            <div className="shop-table-wrapper">
-              <CartTable />
-              <CartCoupon />
+        {cartItems.length > 0 ? (
+          <div className="cart-page-wrapper">
+            <form className="cart-form">
+              <CartProgress />
+              <div className="shop-table-wrapper">
+                <CartTable />
+                <CartCoupon />
+              </div>
+            </form>
+            <div className="cart-collaterals">
+              <CartTotals />
             </div>
-          </form>
-          <div className="cart-collaterals">
-            <CartTotals />
           </div>
-        </div>
+        ) : (
+          <h2> Sepette hiç ürün yok...</h2>
+        )}
       </div>
     </section>
   );
