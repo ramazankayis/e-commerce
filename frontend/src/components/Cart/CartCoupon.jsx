677ecca3f2d3173cbb590693bf69c128795639e7
Ramazan KAYIŞ <kayis.ramazan@gmail.com>
2024-01-24 10:24:14 +0300

    the coupon component updated

--- frontend/src/components/Cart/CartCoupon.jsx
+++ frontend/src/components/Cart/CartCoupon.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 const CartCoupon = () => {
+  const [couponCode, setCouponCode] = useState("");
+
+  const applyCoupon = (e) => {
+  //  e.preventDefault();
+    console.log("couponCode", couponCode);
+  };
+
   return (
     <div className="actions-wrapper">
       <div className="coupon">
-        <input type="text" className="input-text" placeholder="Coupon code" />
-        <button className="btn">Apply Coupon</button>
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Coupon code"
+          onChange={(e) => setCouponCode(e.target.value)}
+          value={couponCode}
+        />
+        <button type="button" onClick={applyCoupon} className="btn">
+          Kupon uygula
+        </button>
       </div>
       <div className="update-cart">
         <button className="btn">Update Cart</button>
