018093630ed5da7c303b034f15ade21d6d534028
Ramazan KAYIŞ <kayis.ramazan@gmail.com>
2024-01-25 17:17:50 +0300

    stripe system added and updated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,6 +8,7 @@ const productRoute = require("./products.js");
 const authRoute = require("./auth.js");
 const couponRoute = require("./coupons.js");
 const userRoute = require("./users.js");
+const paymentRoute = require("./payment.js");
 
 // her rotayı ilgili yol altında kullanıyoruz
 router.use("/categories", categoryRoute);
@@ -15,5 +16,6 @@ router.use("/products", productRoute);
 router.use("/auth", authRoute);
 router.use("/coupons", couponRoute);
 router.use("/users", userRoute);
+router.use("/payment", paymentRoute);
 
 module.exports = router;
