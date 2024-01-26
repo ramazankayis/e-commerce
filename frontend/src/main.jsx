b625655e20cfd68b7c278dad44615de776fbe5ea
Ramazan KAYIŞ <kayis.ramazan@gmail.com>
2024-01-26 17:05:28 +0300

    scroll adjustment was  made

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CartProvider from "./context/CartProvider.jsx";
-import  {LayoutDefault}  from "./Layouts/Layout.jsx";
+import { LayoutDefault } from "./Layouts/Layout.jsx";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <CartProvider>
-      <LayoutDefault>
-        <App />
-      </LayoutDefault>
-    </CartProvider>
+    <ScrollToTop/>
+      <CartProvider>
+        <LayoutDefault>
+          <App />
+        </LayoutDefault>
+      </CartProvider>
+     
   </BrowserRouter>
 );
